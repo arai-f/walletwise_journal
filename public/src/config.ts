2d0e4679bc7d66f20c4fdb85fca3a6f217eaaf2b
Fumiya ARAI <araifumiya1102@gmail.com>
2026-09-13 00:16:13 +0900

    chore(release): v1.13.3

--- public/src/config.ts
+++ public/src/config.ts
@@ -55,7 +55,7 @@ export const config: TemplateConfig = {
 	// クレジットカードの締め日と支払日のルール。キーは口座ID、値はルールオブジェクト。
 	creditCardRules: {},
 	// アプリケーションのバージョン
-	appVersion: "1.13.2",
+	appVersion: "1.13.3",
 	// 利用規約のバージョン
 	termsVersion: "1.1.0",
 	// ガイドのバージョン
